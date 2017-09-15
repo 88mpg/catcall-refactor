6bc9a1c1a39a97e67be16ec3561b9fff2b31ee0c
Adam Fratino <adafrati@usnyclbw4784.global.publicisgroupe.net>
2017-09-15 16:43:02 -0400

    ¯\_(ツ)_/¯

--- _src/assets/javascripts/main.js
+++ _src/assets/javascripts/main.js
@@ -1,2 +1,25 @@
-const fastclick = require('fastclick')
-fastclick.attach(document.body)
+import jump from 'jump.js'
+
+const hero = document.querySelector('.hero')
+const overlay = document.querySelector('.overlay')
+const navigation = document.querySelector('.navigation')
+const heroButton = hero.querySelector('.hero--learn_more')
+const topButton = document.querySelector('.footer--top')
+
+let offset = navigation.clientHeight + overlay.clientHeight
+console.log(offset);
+
+heroButton.addEventListener('click', e => {
+  e.preventDefault()
+  jump(hero.nextElementSibling, {
+    duration: 250,
+    offset: -(offset)
+  })
+})
+
+topButton.addEventListener('click', e => {
+  e.preventDefault()
+  jump('body', {
+    duration: 250
+  })
+})
